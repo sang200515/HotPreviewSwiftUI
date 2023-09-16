714bc73fe4b5ed1081f3bfd2b50078325edd59fe
sangtt18 <SangTt18@fpt.com.vn>
2023-09-16 15:54:57 +0700

    init

--- Tests/HotPreviewSwiftUITests/HotPreviewSwiftUITests.swift
+++ Tests/HotPreviewSwiftUITests/HotPreviewSwiftUITests.swift
@@ -1,11 +1,2 @@
 import XCTest
 @testable import HotPreviewSwiftUI
-
-final class HotPreviewSwiftUITests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(HotPreviewSwiftUI().text, "Hello, World!")
-    }
-}
